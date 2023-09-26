6a1c46c94a637dd0b565ba8ec6b05f84b86351b9
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-09-26 08:43:40 -0300

    Instalei e importei pandas

--- codigo.py
+++ codigo.py
@@ -2,7 +2,7 @@
 # Passo a passo do projeto
 # Passo 1: Entrar no sistema da empresa
     #https://dlp.hashtagtreinamentos.com/python/intensivao/login
-import pyautogui
+import pyautogui #para instalar : pip install pyautogui
 import time
 
 # pyautogui.click -> clicar com o mouse
@@ -36,7 +36,13 @@ pyautogui.press("tab")
 pyautogui.press("enter")
 
 time.sleep(3)
+
 # Passo 3: Importar a base de dados de produtos
+# Pandas: pip install pandas numpy openpyxl
+import pandas
+
+
+
 # Passo 4: Cadastrar 1 produto
 # Passo 5: Repetir o cadastro para todos os produtos
 
