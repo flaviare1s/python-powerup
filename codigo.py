a51ed8179ffe0b011652c1fd4268c61903b29a54
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-09-26 08:13:32 -0300

    Coloquei alguns códigos

--- codigo.py
+++ codigo.py
@@ -2,11 +2,6 @@
 # Passo a passo do projeto
 # Passo 1: Entrar no sistema da empresa
     #https://dlp.hashtagtreinamentos.com/python/intensivao/login
-# Passo 2: Fazer login
-# Passo 3: Importar a base de dados de produtos
-# Passo 4: Cadastrar 1 produto
-# Passo 5: Repetir o cadastro para todos os produtos
-
 import pyautogui
 
 # pyautogui.click -> clicar com o mouse
@@ -14,7 +9,28 @@ import pyautogui
 # pyautogui.press -> apertar 1 tecla
 # pyautogui.hotkey -> atalho (combinação de teclas)
 
+pyautogui.PAUSE = 0.5
+
+# Abrir o Chrome
 pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+# Entrar no link
+link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+pyautogui.write(link)
+pyautogui.press("enter")
+
+# Esperar o site carregar
+
+# Passo 2: Fazer login
+# Passo 3: Importar a base de dados de produtos
+# Passo 4: Cadastrar 1 produto
+# Passo 5: Repetir o cadastro para todos os produtos
+
+
+
+
 
 
 
