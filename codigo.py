bcb2cbb0c376475561cb2827824ac4c149a37946
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-09-26 07:20:56 -0300

    Escrevi o passo a passo em português

---
+++ codigo.py
@@ -0,0 +1,7 @@
+# Passo a passo do projeto
+# Passo 1: Entrar no sistema da empresa
+    #https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Passo 2: Fazer login
+# Passo 3: Importar a base de dados de produtos
+# Passo 4: Cadastrar 1 produto
+# Passo 5: Repetir o cadastro para todos os produtos
