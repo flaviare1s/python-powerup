7b4d72fd4817fa0c01dad7369c1a3c8d0497ae20
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-09-26 07:49:14 -0300

    Importei pyautogui

--- codigo.py
+++ codigo.py
@@ -1,3 +1,4 @@
+#Olá, Mundo!
 # Passo a passo do projeto
 # Passo 1: Entrar no sistema da empresa
     #https://dlp.hashtagtreinamentos.com/python/intensivao/login
@@ -5,3 +6,17 @@
 # Passo 3: Importar a base de dados de produtos
 # Passo 4: Cadastrar 1 produto
 # Passo 5: Repetir o cadastro para todos os produtos
+
+import pyautogui
+
+# pyautogui.click -> clicar com o mouse
+# pyautogui.write -> escrever um texto
+# pyautogui.press -> apertar 1 tecla
+# pyautogui.hotkey -> atalho (combinação de teclas)
+
+pyautogui.press("win")
+
+
+
+
+
